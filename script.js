6d1a6e0446f6871406a53685097ff8eb8ca5c28f
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-11-29 14:05:36 +0000

    using class declaration instead of constructor

--- script.js
+++ script.js
@@ -38,11 +38,13 @@ function getLocalLibraryStorage(){
     return [];
 }
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;        
+    }
 }
 
 function addBookToLibrary(title, author, pages, read){
